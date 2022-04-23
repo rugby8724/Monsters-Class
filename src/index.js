8a685a0e8fc65b93e1b51e0d0b1d96082d164da1
Jeremiah <jeremiah.d.wise@gmail.com>
2022-04-23 18:55:33 -0500

    Class components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+// react is the underlying engine for how react runs as an application builder
 import React from 'react';
-import ReactDOM from 'react-dom';
+// reactDom is specifying this app is for web applications
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+  // where to render to file. here we are rendering it in the html file with an id of root
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  // In short, StrictMode helps to identify unsafe, legacy, or deprecated APIs/lifecycles. It's used for highlighting possible problems. Since it's a developer tool, it runs only in development mode. It renders every component inside the web app twice in order to identify and detect any problems in the app and show warning messages.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
